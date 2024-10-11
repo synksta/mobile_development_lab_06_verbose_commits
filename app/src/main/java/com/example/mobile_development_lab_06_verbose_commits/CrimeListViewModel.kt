0f785b83d95273bcd7bf4ebc00e88905aeca8c94
synksta <contriturbamentum@gmail.com>
2024-10-11 19:18:13 +0700

    Chapter 8,9 - Updated CrimeListViewModel.kt

--- app/src/main/java/com/example/mobile_development_lab_06_verbose_commits/CrimeListViewModel.kt
+++ app/src/main/java/com/example/mobile_development_lab_06_verbose_commits/CrimeListViewModel.kt
@@ -4,4 +4,14 @@ import androidx.lifecycle.ViewModel
 
 class CrimeListViewModel : ViewModel() {
     val crimes = mutableListOf<Crime>()
+
+    init {
+        for (i in 0 until 100) {
+            val crime = Crime(
+                title = "Crime #$i",
+                isSolved = i % 2 == 0,
+            )
+            crimes += crime
+        }
+    }
 }
