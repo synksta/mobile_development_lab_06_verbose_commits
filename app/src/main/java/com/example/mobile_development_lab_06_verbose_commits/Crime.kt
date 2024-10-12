0a02d18beca969f4d7db39795ca590933d2d15ac
synksta <contriturbamentum@gmail.com>
2024-10-12 18:26:18 +0700

    Chapter 10, 11, 12 - Changed Crime.kt

--- app/src/main/java/com/example/mobile_development_lab_06_verbose_commits/Crime.kt
+++ app/src/main/java/com/example/mobile_development_lab_06_verbose_commits/Crime.kt
@@ -6,5 +6,4 @@ import java.util.UUID
 data class Crime(val id: UUID = UUID.randomUUID(),
                  var title: String = "",
                  var date: Date = Date(),
-                 var isSolved: Boolean = false,
-                 var requiresPolice: Boolean = false)
+                 var isSolved: Boolean = false)
